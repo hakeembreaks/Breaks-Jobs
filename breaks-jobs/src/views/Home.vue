e6bdba9f79dd5b8333cea8a03945ffddf55b1184
hakeembreaks <kharkim@gmail.com>
2022-08-26 08:01:08 +0100

    first commit

---
+++ breaks-jobs/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+   <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae libero incidunt illo eligendi recusandae repudiandae non ipsum est aliquid officia quos ducimus facere, exercitationem consequatur in quisquam voluptate commodi. Beatae ullam nobis earum neque tenetur consequuntur aliquid illum suscipit harum reprehenderit, provident voluptate impedit, eligendi saepe esse, assumenda mollitia accusantium ut culpa. Ut dolores rem maiores. Consequatur, tenetur? Quam alias corrupti, fuga possimus assumenda aut. Vitae nam praesentium explicabo nisi quidem, nihil veniam quia iure, atque tempore dolor? Nobis voluptates perspiciatis voluptatem eius fuga animi, praesentium dolorum, ducimus, incidunt voluptatibus itaque. Et adipisci itaque voluptatibus consequatur quis ab quo tenetur!</p> 
+   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae libero incidunt illo eligendi recusandae repudiandae non ipsum est aliquid officia quos ducimus facere, exercitationem consequatur in quisquam voluptate commodi. Beatae ullam nobis earum neque tenetur consequuntur aliquid illum suscipit harum reprehenderit, provident voluptate impedit, eligendi saepe esse, assumenda mollitia accusantium ut culpa. Ut dolores rem maiores. Consequatur, tenetur? Quam alias corrupti, fuga possimus assumenda aut. Vitae nam praesentium explicabo nisi quidem, nihil veniam quia iure, atque tempore dolor? Nobis voluptates perspiciatis voluptatem eius fuga animi, praesentium dolorum, ducimus, incidunt voluptatibus itaque. Et adipisci itaque voluptatibus consequatur quis ab quo tenetur!</p>
+  </div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
